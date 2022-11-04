82350112d80bc03f953d8d00e44ee78d9840648c
ShafiqSadat <shafiqsadat2012@gmail.com>
2022-11-04 20:48:57 +0430

    waiting for results!

--- src/main/java/KanKor.java
+++ src/main/java/KanKor.java
@@ -100,7 +100,7 @@ public class KanKor extends TelegramLongPollingBot {
                 redis.del("waitForGrandFatherNameForId"+userID);
             }
             else if (messageText.equals("دریافت نتیجه \uD83D\uDCDC")){
-                boolean not = false;
+                boolean not = true;
                 if (not){
                     sendMessage(userID,"کاربر عزیز نتایج به زودی آپلود میشود لطفا صبور باشید.",messageID,null);
                     return;
@@ -113,7 +113,7 @@ public class KanKor extends TelegramLongPollingBot {
                         "\n" +
                         "زبان برنامه نویسی : جاوا\n" +
                         "\n" +
-                        "نسخه : 0.3";
+                        "نسخه : 1.0";
                 sendMessage(userID,text,messageID,sendKeyBoard());
             }
             else if (update.getMessage().hasText()){
@@ -240,7 +240,7 @@ public class KanKor extends TelegramLongPollingBot {
         String[] numbersFA = {"۰","۱","۲","۳","۴","۵","۶","۷","۸","۹"};
         String[] numbersEN = {"0","1","2","3","4","5","6","7","8","9"};
         for (int i = 0; i < numbersFA.length; i++) {
-            text.replaceAll(numbersFA[i],numbersEN[i]);
+            text = text.replaceAll(numbersFA[i],numbersEN[i]);
         }
         return text;
     }
